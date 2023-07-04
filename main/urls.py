d8de98ebd24dee6a6e12aa970a923b1059c568bb
Ufifus <artemnesteihyb@mail.ru>
2023-07-04 08:50:11 +0300

    update url 3

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('search/', CreateTaskView.as_view(), name='index'),
+    path('', CreateTaskView.as_view(), name='index'),
     path('articles/', CheckStatusTaskView.as_view(), name='check'),
     path('graphs/', GetGraphData.as_view(), name='graph'),
 ]
