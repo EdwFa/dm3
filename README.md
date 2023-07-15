c3b7ad1314c866a51a4513e9b30b671ff12273ab
Ufifus <artemnesteihyb@mail.ru>
2023-07-15 20:20:55 +0300

    update run

---
+++ README.md
@@ -0,0 +1,6 @@
+# dm3
+Sechenov Datamed AI <br /> 
+pip install -r requirementts.txt <br />
+python main.py <br />
+Запутится по :5000 порту <br />
+
--- main.py
+++ main.py
@@ -11,6 +11,9 @@ app = Flask(__name__, static_url_path='')
 app.register_blueprint(analise)
 app.logger.name = 'BertTopicApi'
 Entrez.email = os.getenv('PARSER_EMAIL')  # Говорю NCBI кто я есть
+address_host = os.getenv('HOST')
+address_port = os.getenv('PORT')
+
 
 nltk.download('stopwords')
 nltk.download('punkt')
@@ -21,4 +24,4 @@ if __name__ == '__main__':
     app.logger.info('-----------------')
     app.logger.info('Starting...')
     app.logger.info(f'GPU in system = {check_device()}')
-    app.run(debug=True)
+    app.run(debug=True, host=address_host, port=address_port)
