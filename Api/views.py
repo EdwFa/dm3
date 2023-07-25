8df8b070d51d06f356f248f62dbf1003dd21343d
Ufifus <artemnesteihyb@mail.ru>
2023-07-25 11:05:30 +0300

    update summarise

--- Api/views.py
+++ Api/views.py
@@ -66,3 +66,50 @@ def summarise_records():
     current_app.logger.info(f'Done analise!')
 
     return jsonify(data), 200
+
+@sum.route('/summarise_emb', methods=['POST'])
+def summarise_emb():
+    """
+    -> Receive pmids from main server
+    -> Get all article from pmid
+    -> Analise articles in BertTopic
+    -> Use learned model for print graphics
+    -> """
+    current_app.logger.info(f'Start summarise text...')
+
+    data = request.json
+    if not ('text' in data):
+        return jsonify({'status': 'Error', 'message': 'Text not found'}), 500
+    raw_text = data['text']
+
+    current_app.logger.info(f'Делим документ на предложения...')
+    sentences = nltk.sent_tokenize(raw_text)
+    print("Num sentences:", len(sentences))
+
+    current_app.logger.info(f'Формируем эмбединги по предложениям...')
+    embeddings = model.encode(sentences, convert_to_tensor=True)
+
+    current_app.logger.info(f'Считаем косинусную близость для пар предложений...')
+    cos_scores = util.cos_sim(embeddings, embeddings).numpy()
+
+    current_app.logger.info(f'Центральные метрики предложений...')
+    centrality_scores = degree_centrality_scores(cos_scores, threshold=None)
+
+    current_app.logger.info(f'Сортируем предложения с наивысшими далами в начало списка для последующих срезов...')
+    most_central_sentence_indices = np.argsort(-centrality_scores)
+
+    current_app.logger.info(f'Саммари из 10 предложений...')
+    most_central_sentences = []
+    print("\n\nSummary:")
+    for idx in most_central_sentence_indices[0:10]:
+        print(sentences[idx].strip())
+        most_central_sentences.append(sentences[idx].strip())
+
+    data = {
+        'status': 'Done!',
+        'summary': most_central_sentences
+    }
+
+    current_app.logger.info(f'Done analise!')
+
+    return jsonify(data), 200
